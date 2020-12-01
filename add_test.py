484b4d8db29bee590c559c32029223435948700f
rohan37-pro <75291986+rohan37-pro@users.noreply.github.com>
2020-12-01 16:55:15 +0530

    while looping added

--- add_test.py
+++ add_test.py
@@ -1,7 +1,10 @@
 #this is my first python programme on github
 
-a = int.input('number one  :')
+while ab=='yes':
+ 	a = int.input('number one  :')
 
-b = int.input('number tow  :')
+  b = int.input('number tow  :')
 
-print('answer is  ----->  ',a+b)
+  print('answer is  ----->  ',a+b)
+  ab = input('do you want to add again  :')
+  
