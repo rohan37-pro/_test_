d32fd252c346ad9d2434f1a13adace458a20364a
rohan37-pro <rohan2002maji@gmail.com>
2023-04-29 14:22:48 +0530

    animation.py is uploaded

--- add_test.py
+++ add_test.py
@@ -1,10 +1,10 @@
 #this is my first python programme on github
 
-a = int.input('number one  :')
+a = int(input('number one  :'))
 
-b = int.input('number tow  :')
+b = int(input('number tow  :'))
 
 print('answer is  ----->  ',a+b)
 
-print('addition completed')
+print('addition completed......')
 
---
+++ mul.py
@@ -0,0 +1,5 @@
+a = int(input("enter a num : "))
+b = int(input("enter 2nd num :"))
+
+print(f"multiplying {a} and {b} = {a*b}")
+print("muliplication complete/...")
